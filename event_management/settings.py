71775e1d03a3d103cf06b39e81c692cd27489587
Shihab <mohammadshihabuddin7@gmail.com>
2026-03-29 14:13:37 +0600

    csrf trusted origins changed

--- event_management/settings.py
+++ event_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['http://*.onrender.com','http://127.0.0.1:8000','https://event-management-gqxs.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['http://*.onrender.com','http://127.0.0.1:8000','https://event-management-gqxs.onrender.com','https://event-management-1-7faj.onrender.com']
 
 AUTH_USER_MODEL = 'users.CustomUser'
 # Application definition
